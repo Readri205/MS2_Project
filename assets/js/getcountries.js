e263c2724bdebd11fef150d234a25ac8e66efad8
Readri205 <65308512+Readri205@users.noreply.github.com>
2020-08-19 16:20:27 +0100

    Amend syntax on error message xhr to xhttp

--- assets/js/getcountries.js
+++ assets/js/getcountries.js
@@ -20,7 +20,7 @@ function getDataCountry(cb) {
 	xhttp.setRequestHeader("Access-Control-Allow-Origin", baseUrlcountrys);
 	xhttp.setRequestHeader("x-rapidapi-key", "3862ea89d4msh2f04423d9b95ad8p18b07bjsn4fdaaa32a1f6");
 	xhttp.send();
-	xhr.onerror = function() {
+	xhttp.onerror = function() {
 		alert("Oops, the API return failed - please try again later, but please head over to the contact page and drop us an email so we can look at the error");
 	  };
 }
